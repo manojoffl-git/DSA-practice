b453d012272f8728d3ca6c30219653dde31e37cb
manojoffl-git <manojoffl19@gmail.com>
2026-08-02 21:12:44 +0530

    i solved problem 58

--- problem_58.py
+++ problem_58.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        i = len(s) - 1
-
-        # Skip trailing spaces
-        while i >= 0 and s[i] == " ":
-            i -= 1
-
-        length = 0
-
-        # Count the last word
-        while i >= 0 and s[i] != " ":
-            length += 1
-            i -= 1
-
-        return length
+        """
+        :type s: str
+        :rtype: int
+        """
+        word = s.split()
+        return len(word.pop())

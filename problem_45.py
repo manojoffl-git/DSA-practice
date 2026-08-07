108bf7be5a7475f2bdab40a8f9fde65dafa24ab5
manojoffl-git <manojoffl19@gmail.com>
2026-08-07 14:23:50 +0530

    i solved 45th problem

---
+++ problem_45.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            far = 0
+            for i in range(l, r+1):
+                far = max(far, i+nums[i])
+            l = r+1
+            r = far
+            res += 1
+        return res
